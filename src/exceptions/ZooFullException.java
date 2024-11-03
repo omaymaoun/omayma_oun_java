28fbdffc29b40175f28d7b5c603748024d00dcda
omayma <omayma.oun@esprit.tn>
2024-11-03 23:11:02 +0100

    seven commit

---
+++ src/exceptions/ZooFullException.java
@@ -0,0 +1,9 @@
+package tn.esprit.gestionzoo.exceptions;
+package tn.esprit.gestionzoo.exceptions.ZooFullException;
+
+ class ZooFullException extends Exception {
+     public ZooFullException (String message){
+            super(message);
+
+        }
+}
