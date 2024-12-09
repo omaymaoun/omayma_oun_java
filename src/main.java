9fcd142c62ea8446ab1e706e70574e63e42a3a23
omaymaoun <131709586+omaymaoun@users.noreply.github.com>
2024-12-09 19:07:26 +0100

    Update main.java

--- src/main.java
+++ src/main.java
@@ -19,17 +19,16 @@ public class main {
 
         Student_management st = new Student_management();
 
-        //lambda
+        
         Consumer<Etudient> con = s -> System.out.println(s);
         st.displayStudents(students, con);
         //reference method
         st.displayStudents(students, System.out::println);
 
-        //lambda
+     
         Predicate<Etudient> pre = t -> t.getAge() > 22;
         st.displayStudentsByFilter(students, pre, con);
 
-        //lambda
         Function<Etudient, String> fun = e -> e.getNom();
         String names = st.returnStudentsNames(students, fun);
         System.out.println(names);
@@ -37,7 +36,7 @@ public class main {
         String names1 = st.returnStudentsNames(students, Etudient::getNom);
         System.out.println(names1);
 
-        //lambda (create empty student object)
+       
         Supplier<Etudient> sup = () -> new Etudient();
         Etudient student = st.createStudent(sup);
         System.out.println(student);
@@ -46,23 +45,23 @@ public class main {
         Etudient student1 = st.createStudent(sup);
         System.out.println(student1);
 
-        //reference method (create empty student object)
+        
         Supplier<Etudient> sup2 = Etudient::new;
         Etudient student2 = sup2.get();
         System.out.println(student2);
 
-        /*//reference method (create parametrized student object)
+        
         TripleFunction<Integer, Integer, String, Etudient> tf = Etudient::new;
         Etudient student3 = tf.convert(11, 23, "Amy");
         System.out.println(student3);
 
 
-        //lambda
+     
         Comparator<Etudient> com = (a, b) -> a.getId() - b.getId();
         List<Etudient> students1 = st.sortStudentsById(students, com);
         System.out.println(students1);
 
-         */
+         
 
     }
 }
